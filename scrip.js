f52473d96c51cbdeabcbf1ac8faf65c9c510e09f
edelros <edelros1119@gmail.com>
2026-05-22 22:55:13 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,3 +1,16 @@
-document.querySelectorAll('.anim').forEach(el=>{
-  el.classList.add('visible');
+const elements = document.querySelectorAll("section, .card, .porque-card, .historia-box, .pro-box");
+
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+},{
+  threshold:0.2
+});
+
+elements.forEach(el=>{
+  el.classList.add("hidden");
+  observer.observe(el);
 });
